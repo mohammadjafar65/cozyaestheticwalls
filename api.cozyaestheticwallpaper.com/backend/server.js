63fdec17b514563c389943c4b54bcfcc3de57738
Mohammad Jafar <itzafar65@gmail.com>
2024-10-29 01:10:38 +0530

    server changes

--- api.cozyaestheticwallpaper.com/backend/server.js
+++ api.cozyaestheticwallpaper.com/backend/server.js
@@ -26,12 +26,14 @@ connection.connect((err) => {
 
 // Middleware
 // app.use(cors()); // Enable CORS for all routes
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Change * to your specific origin if needed
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+// CORS configuration
+const corsOptions = {
+  origin: 'https://www.cozyaestheticwallpaper.com', // Your frontend domain
+  methods: 'GET, POST, PUT, DELETE', 
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json()); // For parsing application/json
 app.use("/uploads", express.static("uploads")); // Serve static files from the "uploads" folder
 
